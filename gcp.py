511472c6ec193ec45af756b3af0ecfee81f60935
Abdul Majeed <majeed_rayyan@yahoo.com>
2023-11-17 16:05:50 +0530

    First commit to the vertex ai pipelines repo

---
+++ gcp.py
@@ -0,0 +1,11 @@
+from google.cloud import storage
+
+# Create a Storage client
+client = storage.Client()
+print(client)
+# List all the buckets
+buckets = client.list_buckets()
+
+# Print the name of each bucket
+for bucket in buckets:
+    print(bucket.name)
